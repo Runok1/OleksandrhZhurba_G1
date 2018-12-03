1ef55af495c21fc2434b2cc304e1552af861dcab
Oleksandr Zhurba <runok.al@gmail.com>
2018-12-03 20:14:39 +0200

    Add project files.

---
+++ OleksandrZhurba/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace OleksandrZhurba
+{
+   
+    public class Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Ok");
+        }
+    }
+}
